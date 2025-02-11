8b1744b5e1d6350b9bacde8eb944a1369de4a70b
Angel <angelfernandezblanco16@gmail.com>
2025-02-11 10:52:06 +0100

    Creo un fichero .css para guardar los estilos y creo el indicador de ventana para que el usuario sepa en que ventana se encuentra

--- src/main/java/net/liceo/afb/magic_coleccioncartas/ControllerMenu.java
+++ src/main/java/net/liceo/afb/magic_coleccioncartas/ControllerMenu.java
@@ -2,13 +2,17 @@ package net.liceo.afb.magic_coleccioncartas;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 
 public class ControllerMenu {
     @FXML
-    private Label welcomeText;
+    public HBox HBoxMenu;
+    @FXML
+    private Label MenuTxt;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void initialize() {
+
     }
 }
