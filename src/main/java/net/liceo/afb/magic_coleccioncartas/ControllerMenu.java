3390d955665b261bfc08b6dcffbb5f86bce4d095
Angel <angelfernandezblanco16@gmail.com>
2025-02-11 17:00:58 +0100

    Oculto datos de la carta hasta que prsiona el boton

--- src/main/java/net/liceo/afb/magic_coleccioncartas/ControllerMenu.java
+++ src/main/java/net/liceo/afb/magic_coleccioncartas/ControllerMenu.java
@@ -2,6 +2,7 @@ package net.liceo.afb.magic_coleccioncartas;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextField;
@@ -12,15 +13,28 @@ public class ControllerMenu {
     @FXML
     public HBox HBoxMenu;
     @FXML
-    public Label NombreCarta;
-    public TextField TextFieldMana, TextFieldTipo;
+    public Label NombreCarta, LabelMana;
+    public TextField TextFieldMana;
     public VBox VBoxCarta;
     public MenuButton ColoresMenu, TipoMenu;
+    public Button InsertCarta, AgregarCarta;
 
     Log log = new Log();
 
 
     public void PedirDatosCarta(ActionEvent event) {
+        System.out.println("Pedir datos para añadir carta");
+        
+
+            //Muestro los datos que tiene que rellenar el usuario para añadir la carta a la basae de datos
+        VBoxCarta.setManaged(true);
+        NombreCarta.setManaged(true);
+        ColoresMenu.setManaged(true); ColoresMenu.setVisible(true);
+        VBoxCarta.setManaged(true);
+        LabelMana.setManaged(true);
+        TextFieldMana.setManaged(true);
+        TipoMenu.setManaged(true); TipoMenu.setVisible(true);
+        InsertCarta.setManaged(true);
 
     }
 }
--- src/main/java/net/liceo/afb/magic_coleccioncartas/Log.java
+++ src/main/java/net/liceo/afb/magic_coleccioncartas/Log.java
@@ -2,4 +2,9 @@ package net.liceo.afb.magic_coleccioncartas;
 
 public class Log {
     public Log() {}
+
+
+
+
+
 }
